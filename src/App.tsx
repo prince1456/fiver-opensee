e8fda06a992550cb9add7a434011178eadab5be1
Ali  Alizada <akbar.alizadeh5@gmail.com>
2021-04-05 23:14:25 -0700

    update iframe

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ class App extends React.Component {
             </p>
             <small>Digital collectibles from my company</small>
           </div>
-          <iframe id="opensea-iframe" title="Embedded OpenSea Marketplace" src={embeddedUrl} width='100%' height='100%' frameBorder='0' allowFullScreen></iframe>
+          <iframe id="opensea-iframe" title="Embedded OpenSea Marketplace" src={embeddedUrl} width='100%' height='100vh' frameBorder='0' scrolling="auto"  style={{minHeight: '200vh', pointerEvents: 'auto'}}></iframe>
         </main>
         <footer className="App-footer">
           {/* Placeholder image for footer */}
--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const ASSET_URL =
-  "https://opensea.io/assets/0x1301566b3cb584e550a02d09562041ddc4989b91/28";
+  "https://opensea.io/assets?embed=true";
 
 export enum EmbedTheme {
   Default = "true",
